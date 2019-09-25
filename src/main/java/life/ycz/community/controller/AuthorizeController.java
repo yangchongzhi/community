4203e4eaef899887e550680f6753ccf22075c514
yangchongzhi <1361394032@qq.com>
2019-09-25 19:52:52 +0800

    新建数据库，保存登录状态

--- src/main/java/life/ycz/community/controller/AuthorizeController.java
+++ src/main/java/life/ycz/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.ycz.community.controller;
 
 import life.ycz.community.dto.AccessTokenDTO;
 import life.ycz.community.dto.GithubUser;
+import life.ycz.community.mapper.UserMapper;
+import life.ycz.community.model.User;
 import life.ycz.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,7 +28,8 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
-
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
@@ -38,8 +42,15 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user!=null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             request.getSession().setAttribute("user", user);
             return "redirect:/";
         }else {
--- src/main/java/life/ycz/community/provider/GithubProvider.java
+++ src/main/java/life/ycz/community/provider/GithubProvider.java
@@ -6,8 +6,6 @@ import life.ycz.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
