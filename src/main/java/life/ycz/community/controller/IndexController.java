e26202de85ab21b8ebc2e410acfc0420b3418b46
yangchongzhi <1361394032@qq.com>
2019-09-26 10:06:10 +0800

    cookie保存token，保持登录状态

--- src/main/java/life/ycz/community/controller/IndexController.java
+++ src/main/java/life/ycz/community/controller/IndexController.java
@@ -1,16 +1,36 @@
 package life.ycz.community.controller;
 
+import life.ycz.community.mapper.UserMapper;
+import life.ycz.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Autowired
+    UserMapper userMapper;
 
-    @GetMapping("/index")
-    public String hello(){
+    @GetMapping("/")
+    public String hello(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null){
+            for (int i = 0; i < cookies.length; i++) {
+                if (cookies[i].getName().equals("token")){
+                    User user = userMapper.findByToken(cookies[i].getValue());
+                    if (user != null){
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 
